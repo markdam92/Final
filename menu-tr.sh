07306314eca2e2b7755711f16ee4564f85b53586
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-16 07:23:31 +0800

    Update menu-tr.sh

--- menu-tr.sh
+++ menu-tr.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
+red='\e[1;31m'
+green='\e[0;32m'
 NC='\e[0m'
-
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
 clear
+figlet -f small Panel Trojan | lolcat
 echo -e ""
-echo -e "${BLUE}========================== ${RED}Menu Trojan ${BLUE}==========================${NC}"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "* [1]  : Create Trojan Account" | lolcat
-echo -e "* [2]  : Deleting Trojan Account" | lolcat
-echo -e "* [3]  : Extending Trojan Account Active Life" | lolcat
-echo -e "* [4]  : Check User Login Trojan" | lolcat
-echo -e ""
-echo -e "${GREEN}=================================================================${NC}"
+echo -e "     [1]  Create Trojan Account"
+echo -e "     [2]  Delete Trojan Account"
+echo -e "     [3]  Renew Trojan Account"
+echo -e "     [4]  Check User Login Trojan"
+echo -e "     [x]  Exit"
+echo -e "======================================" | lolcat
 echo -e ""
 read -p "     Select From Options [1-4 or x] :  " num
 echo -e ""
