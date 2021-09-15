c1ee532cf95354246b7a840558b438b066fc94e7
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-16 07:08:24 +0800

    Update menu-wr.sh

--- menu-wr.sh
+++ menu-wr.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
+red='\e[1;31m'
+green='\e[0;32m'
 NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
 clear
+figlet -f small Panel WG | lolcat
 echo -e ""
-echo -e "${BLUE}========================= ${RED}Menu Wireguard ${BLUE}========================${NC}"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "*[1] : 	Create Wireguard Account" | lolcat
-echo -e "*[2] : 	Delete Wireguard Account" | lolcat
-echo -e "*[3] : 	Check User Login Wireguard" | lolcat
-echo -e "*[4] : 	Extending Wireguard Account Active Life" | lolcat
+echo -e "     [1]  Create Wireguard Account"
+echo -e "     [2]  Delete Wireguard Account"
+echo -e "     [3]  Check User Login Wireguard"
+echo -e "     [4]  Renew Wireguard Account"
+echo -e "     [x]  Exit"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "${GREEN}=================================================================${NC}"
-echo -e ""
-read -p "     Select From Options [1-8 or x] :  " port
+read -p "     Select From Options [1-4 or x] :  " port
 echo -e ""
 case $port in
 1)
