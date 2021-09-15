5815e4851d340530bdda6f2139622f0e8386260a
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-16 06:50:35 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+echo "Start Update"
+#cd /usr/bin
+rm /usr/bin/menu
+wget https://raw.githubusercontent.com/markdam92/Final/main/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && ./install.sh
+rm install.sh
+clear
+echo " Update Successfull"
+echo " Enjoyy"
