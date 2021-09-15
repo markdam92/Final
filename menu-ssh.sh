a6047c5dea90f288837c5e4ab2eca7105a05456d
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-16 07:11:34 +0800

    Update menu-ssh.sh

--- menu-ssh.sh
+++ menu-ssh.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
+red='\e[1;31m'
+green='\e[0;32m'
 NC='\e[0m'
-
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
 clear
+figlet -f small Panel SSH | lolcat
 echo -e ""
-echo -e "${GREEN}=================================================================${NC}"
-echo -e "${BLUE}======================= ${RED}Menu SSH & OpenVPN ${BLUE}======================${NC}"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "* [1]    : Create SSH & OpenVPN Account" | lolcat
-echo -e "* [2]    : Generate SSH & OpenVPN Trial Account" | lolcat
-echo -e "* [3]    : Extending SSH & OpenVPN Account Active Life" | lolcat
-echo -e "* [4]    : Delete SSH & OpenVPN Account" | lolcat
-echo -e "* [5]    : Check User Login SSH & OpenVPN" | lolcat
-echo -e "* [6]    : Daftar Member SSH & OpenVPN" | lolcat
-echo -e "* [7]    : Delete User Expired SSH & OpenVPN" | lolcat
-echo -e "* [8]    : Set up Autokill SSH" | lolcat
-echo -e "* [9]    : Displays Users Who Do Multi Login SSH" | lolcat
-echo -e "* [10]   : Restart Service Dropbear, Squid3, OpenVPN dan SSH" | lolcat
+echo -e "     [1]  Create SSH & OpenVPN Account"
+echo -e "     [2]  Trial Account SSH & OpenVPN"
+echo -e "     [3]  Renew SSH & OpenVPN Account"
+echo -e "     [4]  Delete SSH & OpenVPN Account"
+echo -e "     [5]  Check User Login SSH & OpenVPN"
+echo -e "     [6]  Daftar Member SSH & OpenVPN"
+echo -e "     [7]  Delete User Expired SSH & OpenVPN"
+echo -e "     [8]  Set up Autokill SSH"
+echo -e "     [9]  Cek Users Who Do Multi Login SSH"
+echo -e "     [10] Restart Service Dropbear, Squid3"
+echo -e "     [x]  Exit"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "${GREEN}=================================================================${NC}"
-echo -e ""
-read -p "     Select From Options [1-8 or x] :  " port
+read -p "     Select From Options [1-10 or x] :  " port
 echo -e ""
 case $port in
 1)
