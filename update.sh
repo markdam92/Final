6eecc5d3aa0a3e7e5985cfd9f9e4b3edd529e86e
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-16 19:17:00 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ fi
 echo "Start Update"
 #cd /usr/bin
 rm /usr/bin/menu
-wget https://raw.githubusercontent.com/markdam92/Final/main/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && ./install.sh
+wget https://raw.githubusercontent.com/markdam92/Final/main/update/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && ./install.sh
 rm install.sh
 clear
 echo " Update Successfull"
