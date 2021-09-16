a9ba514cd8b510b3d3215527857a530d7386a6c7
markdam92 <84657722+markdam92@users.noreply.github.com>
2021-09-17 03:22:20 +0800

    Update install.sh

--- update/install.sh
+++ update/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /usr/bin
 wget -q -O menu "https://raw.githubusercontent.com/markdam92/Final/main/update/menu.sh"
-wget -O menu-sssr "https://raw.githubusercontent.com/markdam92/Final/main/update/menu-sssr.sh"
 chmod +x menu && sed -i -e 's/\r$//' menu
-chmod +x menu-sssr && sed -i -e 's/\r$//' menu-sssr
 
